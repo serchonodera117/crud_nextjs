ab42ecad27541367e55a467c1c6ad0218ebc8213
serchonodera117 <sergio.lobo117@gmail.com>
2023-10-03 19:44:59 -0600

    Update: user crud almost finished

--- app/usuarios/page.jsx
+++ app/usuarios/page.jsx
@@ -9,8 +9,7 @@ import "@/components/styles/users.css"
 function Users(){
    const [isLogged,setIsLogged] = useState(true);
     const [users, setUsers] = useState([]);
-
-    let resetedUsers = []
+    const [usersReset, setUserReset] = useState([])
 
 
     useEffect(()=>{
@@ -21,17 +20,52 @@ function Users(){
 
    async function getUsers(){
     let url = "https://fakestoreapi.com/users"
-   await fetch(url)
-        .then(res=>res.json())
-        .then(json=>{
+    try{
+       const response = await fetch(url)
+       const json = await response.json()
             setUsers(json);
-            resetedUsers = json;
-        })
+            setUserReset(json);
+    }
+    catch(err){
+            console.log(err)
+    }    
 }
     function closeSession() {
         localStorage.removeItem('token_session')
         setIsLogged(false);
       }
+
+    function deleteUser(id) {
+        let temp = users.filter(user=>user.id !== id)
+        setUsers(temp) 
+    }
+    function resetUsers(){
+      getUsers()
+    }
+    function orderAsc(){
+        let temp = [...users];
+         temp.sort((prevUser, postUser)=>{
+            return prevUser.id - postUser.id
+        })
+
+        setUsers(temp)
+    }
+    function orderDesc(){
+        let temp = [...users];
+        temp.sort((prevUser, postUser)=>{
+           return  postUser.id - prevUser.id
+       })
+
+
+        setUsers(temp)
+    }
+    function orderByEmail(){
+        let temp = [...users];
+        temp.sort((prevUser, postUser)=>{
+            return  prevUser.email.localeCompare(postUser.email)
+        })
+        setUsers(temp)
+    }
     return (
         <div  className="page">
             {
@@ -57,11 +91,16 @@ function Users(){
                                     <a className="btn-page" href={`/usuarios/${user.id}`}>more info</a>
                                 </div>
                                 <div className="col-right">
+                                    <br></br>
                                     <h3>{user.username}</h3>
+                                    <br></br>
                                     <h3>{user.name.firstname} {user.name.lastname}</h3>
+                                    <br></br>
                                     <h4>{user.email}</h4>
                                     <br></br>
                                     <h5>ID:  {user.id}</h5>
+                                    <br></br>
+                                    <button className="btn-delete" onClick={() =>{deleteUser(user.id)}}>Delete {user.username}</button>
                                 </div>
                             </div>
                         ))
@@ -69,6 +108,20 @@ function Users(){
                 </div>
                 :
                 <div></div>
+            }{
+                (isLogged)?
+                <div className="menu-options">
+                            <a className="btn-option" onClick={resetUsers}>Reset</a>
+                            <a className="btn-option" onClick={orderAsc}> Asc ^</a>
+                            <a className="btn-option" onClick={orderDesc}>Desc v</a>
+                            <a className="btn-option" onClick={orderByEmail}>by Email</a>
+                    <label className="control" htmlFor="show-options">
+                        <span>+</span>
+                    </label>
+                    <input id="show-options" type="checkbox" hidden></input>
+                </div>
+                :
+            <div></div>
             }
         </div>
     )
